9a4c575433a3b09763e5cbcb0f531dbf85dbf5ba
Ivan1Isu <ivan1isusi@gmail.com>
2024-11-27 17:53:30 +0100

    Created FilmService with getFilms, saveFilm and saveFilms method

---
+++ Backend/OMDb/src/main/java/com/api/OMDb/services/FilmService.java
@@ -0,0 +1,42 @@
+package com.api.OMDb.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.OMDb.models.DetailedMovieModel;
+import com.api.OMDb.models.FilmModel;
+import com.api.OMDb.repositories.IFilmRepository;
+
+@Service
+public class FilmService {
+    
+    @Autowired
+    IFilmRepository filmRepository;
+
+    public ArrayList<FilmModel> getFilms() {
+        return (ArrayList<FilmModel>) filmRepository.findAll();
+     }
+
+    public FilmModel saveFilm(FilmModel film) {
+        return filmRepository.save(film);
+    }
+
+    public List<FilmModel> saveFilms(List<DetailedMovieModel> detailedMovies) {
+
+        List<FilmModel> savedFilms = new ArrayList<>();
+
+        for (DetailedMovieModel movie : detailedMovies){
+
+            FilmModel film = new FilmModel(movie.getTitle(), 
+                movie.getYear(), 
+                movie.getImdbID(), 
+                movie.getPlot()
+            );
+            savedFilms.add(this.saveFilm(film));
+        }
+        return savedFilms;
+    }
+}
