ad9e14c5969a11629558ffced11c97587c9a7c6a
Ivan1Isu <ivan1isusi@gmail.com>
2024-11-27 11:32:44 +0100

    Create apiService and Update OMDbApplication

---
+++ Backend/OMDb/src/main/java/com/api/OMDb/services/ApiService.java
@@ -0,0 +1,32 @@
+package com.api.OMDb.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.api.OMDb.config.ApiConfig;
+import com.api.OMDb.models.ApiResponseModel;
+import com.api.OMDb.models.MovieModel;
+
+@Service
+public class ApiService {
+ 
+    private final ApiConfig apiConfig;
+
+    public ApiService(ApiConfig apiConfig) { this.apiConfig = apiConfig; }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public List<MovieModel> getMovies() {
+        
+        String url = apiConfig.getUrl() +"apikey="+ apiConfig.getKey() + "&s=Star+Wars&type=movie";
+
+        ApiResponseModel APIResponse = this.restTemplate.getForObject(url, ApiResponseModel.class);
+
+        return APIResponse.getSearch();
+
+    }    
+        
+}
