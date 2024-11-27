6ba510f3f45631611cf94b18edbc7c99256d1265
Ivan1Isu <ivan1isusi@gmail.com>
2024-11-27 12:45:17 +0100

    Create ApiController GET endpoint to fetch all detailed movies information

--- Backend/OMDb/src/main/java/com/api/OMDb/controllers/ApiController.java
+++ Backend/OMDb/src/main/java/com/api/OMDb/controllers/ApiController.java
@@ -1,5 +1,6 @@
 package com.api.OMDb.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.OMDb.models.DetailedMovieModel;
 import com.api.OMDb.models.MovieModel;
 import com.api.OMDb.services.ApiService;
 
@@ -21,5 +23,17 @@ public class ApiController {
     @GetMapping("/")
     public ResponseEntity<List<MovieModel>> getAllMovies(){
         return new ResponseEntity<>(this.apiService.getMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/detailed")
+    public ResponseEntity<List<DetailedMovieModel>> getAllDetailedMovies(){
+        List<MovieModel> Movies  = this.apiService.getMovies();
+
+        List<DetailedMovieModel> detailedMovies = new ArrayList<DetailedMovieModel>();
+        for (MovieModel movie : Movies){
+            detailedMovies.add(this.apiService.getDetailedMovie(movie.getImdbID()));
+        }
+
+        return new ResponseEntity<>(detailedMovies, HttpStatus.OK);
     }
 }
